d848d9bff7aa30334ce57137c72e359439f3463a
greenjb <greenjb@mail.uc.edu>
2018-01-18 17:44:27 -0500

    creating sprites & 2dbox plugin3

--- main.js
+++ main.js
@@ -38,8 +38,8 @@ function preload() {
 }
 
 function create() {
-  background = g.add.image(0, 0, 'sky_dust').anchor.set(0.5);
-  background.scale.setTo(2, 2);
+  background = g.add.image(0, 0, 'sky_dust');
+  //background.scale.setTo(2, 2);
 
 /*  soil = g.sprite.add(0, 0, 'soil');
   cloud1 = g.sprite.add(0, 0, 'cloud1');
