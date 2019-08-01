67e8b7ee1b5a0b68fcdbccb4899da8a2b7001c13
Dylan <dylan.ward@uc.edu>
2019-08-01 17:37:16 -0400

    Upgraded to Phaser CE 2.13.2 and removed unnecessary package files

--- states/Boot.js
+++ states/Boot.js
@@ -1,7 +1,7 @@
 
 var bootState = {
   create: function(){
-    //g.time.desiredFPS = 30;
+
     g.physics.startSystem(Phaser.Physics.BOX2D);
     g.physics.box2d.restitution = 0.2;
     g.physics.box2d.gravity.y = 250;
